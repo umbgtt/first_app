b04e9f0d2ed92338492d6193b11329e4d2b3498f
umbgtt <umberto.gotti@softwarementor.ch>
2013-10-11 11:56:34 +0100

    Further attempt to fix the production issue

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 FirstApp::Application.initialize!
+
+config.assets.compile = true
+# Heroku also requires this to be false
+config.assets.initialize_on_precompile=false
